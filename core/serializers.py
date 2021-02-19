680bf13f9e7f2c85b5c2ee842340526ebf1b3f66
jeanwainer <wainer@gmail.com>
2021-02-19 02:27:02 -0300

    Validate timestamp

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework import serializers
 
 from .models import Event
@@ -7,3 +8,12 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         exclude = ['saved_date',]
+
+    def validate_timestamp(self, value):
+        """
+        Validate whether timestamp is a valid date (not greater than current date)
+        """
+        if value > datetime.now():
+            raise serializers.ValidationError("Timestamp is invalid")
+
+        return value
