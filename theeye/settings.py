3634a0177c34c3b8a516ecfaf5a928b098016475
jeanwainer <wainer@gmail.com>
2021-02-19 23:02:38 -0300

    Added reusable app (swagger interface)

--- theeye/settings.py
+++ theeye/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'celery',
+    'drf_yasg',
     'core',
 ]
 
--- theeye/urls.py
+++ theeye/urls.py
@@ -1,22 +1,23 @@
-"""theeye URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Events API",
+      default_version='v1',
+      description="Consumer Affairs techincal test API",
+      contact=openapi.Contact(email="wainer@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('theeye/', include('core.urls'))
+    path('theeye/', include('core.urls')),
+    re_path(r'swagger(?P<format>\.json|\.yaml$)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
